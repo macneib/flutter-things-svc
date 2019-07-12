2d58e16657150318c675638b9eb9ee4541eae5ae
Blair MacNeil <blair.macneib@ecompliance.com>
2019-07-12 18:45:15 -0400

    auth is working

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Security.Cryptography;
 using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Tokens;
 
 
 
@@ -29,36 +30,39 @@ namespace flutter_things_svc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-
-            services.AddAuthentication(options =>
+           
+            services.AddAuthentication(o => {
+                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(cfg =>
             {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-
-            }).AddJwtBearer(o =>
-            {
-                o.Authority = Configuration["Jwt:Authority"];
-                o.Audience = Configuration["Jwt:Audience"];
-                o.RequireHttpsMetadata = false;
-
-                o.Events = new JwtBearerEvents()
+                // only for testing
+                cfg.RequireHttpsMetadata = false;
+                cfg.Authority = "https://auth.sftynrd.dev/auth/realms/rusty-firecracker";
+                cfg.IncludeErrorDetails = true;
+                cfg.TokenValidationParameters = new TokenValidationParameters()
                 {
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidIssuer = "https://auth.sftynrd.dev/auth/realms/rusty-firecracker ",
+                    ValidateLifetime = true
+                };
+
+                cfg.Events = new JwtBearerEvents()
+                {
+
                     OnAuthenticationFailed = c =>
                     {
                         c.NoResult();
-                        c.Response.StatusCode = 500;
+                        c.Response.StatusCode = 401;
                         c.Response.ContentType = "text/plain";
-
-                        // if (env.IsDevelopment())
-                        // {
-                        //     return c.Response.WriteAsync(c.Exception.ToString());
-                        // }
-
-                        return c.Response.WriteAsync("An error occured processing your authentication.");
+                     return c.Response.WriteAsync(c.Exception.ToString());
                     }
                 };
             });
-}
+        }
         
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
